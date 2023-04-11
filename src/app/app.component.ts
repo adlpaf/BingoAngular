ec7df1ffeb9c448a1879091d19907c887a5f0dbc
Alfredo Finol <afinol@gmail.com>
2023-04-10 23:09:19 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'BingoAngular';
   number = 0;
-  letter = ''
-  b:number[] = [];
-  i:number[] = [];
-  n:number[] = [];
-  g:number[] = [];
-  o:number[] = [];
+  letter = [
+    "B",
+    "I",
+    "N",
+    "G",
+    "O"
+  ];
+  letterSelected = "";
   bingo:number[] = [];
 
   searchArray(index:number) {
@@ -21,77 +23,28 @@ export class AppComponent {
   }
 
   cleanBoard() {
-    this.b = [];
-    this.i = [];
-    this.n = [];
-    this.g = [];
-    this.o = [];
     this.bingo = [];
     this.number = 0;
-    this.letter = '';
+    this.letterSelected = '';
   }
 
   newNumber() {
+    if (this.bingo.length >= 75) {
+      this.number = 0;
+      this.letterSelected = "ERROR";
+      return;
+    }
     let validNumber = false
     let numberSelected = 0;
-    let letterSelected = 'B';
     let searchNumber: number | undefined = 0;
     do {
-      numberSelected = Math.round(Math.random() * 75) + 1;
-      let selectLetter = Math.floor(numberSelected / 15);
-      
-      switch (selectLetter) {
-        case 0:
-          searchNumber = this.b.find(i => i == numberSelected);
-          validNumber = searchNumber === undefined ? false : true;
-          if (!validNumber) {
-            this.b.push(numberSelected);
-          }
-          letterSelected = 'B';
-          break;
-      
-        case 1:
-          searchNumber = this.b.find(i => i == numberSelected);
-          validNumber = searchNumber === undefined ? false : true;
-          if (!validNumber) {
-            this.i.push(numberSelected);
-          }
-          letterSelected = 'I';
-          break;
-      
-        case 2:
-          searchNumber = this.b.find(i => i == numberSelected);
-          validNumber = searchNumber === undefined ? false : true;
-          if (!validNumber) {
-            this.n.push(numberSelected);
-          }
-          letterSelected = 'N';
-          break;
-      
-        case 3:
-          searchNumber = this.b.find(i => i == numberSelected);
-          validNumber = searchNumber === undefined ? false : true;
-          if (!validNumber) {
-            this.g.push(numberSelected);
-          }
-          letterSelected = 'G';
-          break;
-      
-        case 4:
-          searchNumber = this.b.find(i => i == numberSelected);
-          validNumber = searchNumber === undefined ? false : true;
-          if (!validNumber) {
-            this.o.push(numberSelected);
-          }
-          letterSelected = 'O';
-          break;
-      
-        default:
-        break;
-      }
+      numberSelected = Math.round(Math.random() * 74) + 1;
+      searchNumber = this.searchArray(numberSelected);
+      validNumber = searchNumber === undefined ? false : true;
     } while (validNumber);
     this.bingo.push(numberSelected);
     this.number = numberSelected;
-    this.letter = letterSelected;
+    this.letterSelected = this.letter[Math.floor((numberSelected - 1) / 15)];
   }
 }
+
